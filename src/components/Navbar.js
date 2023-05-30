a35a7745622198a13ba5f8bf5a89b78285cafad7
lukeschlosser <jonlukeschlosser@gmail.com>
2023-05-29 21:07:09 -0400

    Changed isMobile menu to speed-dial comp

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,69 +1,97 @@
 import React, { useState } from 'react';
 import { Link } from 'react-scroll';
-import { AppBar, Toolbar, Button, Box, useMediaQuery, Menu, MenuItem, ListItemText, useTheme, IconButton } from '@mui/material';
+import { AppBar, Toolbar, Button, Box, useMediaQuery, useTheme } from '@mui/material';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import HomeIcon from '@mui/icons-material/Home';
+import AccountTree from '@mui/icons-material/AccountTree';
+import ContactIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
-  const handleMenuToggle = () => {
-    setMenuOpen(!menuOpen);
+  const [speedDialOpen, setSpeedDialOpen] = useState(false);
+
+  const handleSpeedDialOpen = () => {
+    setSpeedDialOpen(true);
   };
 
-  const closeMenu = () => {
-    setMenuOpen(false);
+  const handleSpeedDialClose = () => {
+    setSpeedDialOpen(false);
   };
 
   const navLinks = [
-    { name: 'About', link: 'home' },
-    { name: 'Projects', link: 'projects' },
-    { name: 'Contact', link: 'contact' },
+    { name: 'About', link: 'home', icon: <HomeIcon /> },
+    { name: 'Projects', link: 'projects', icon: <AccountTree /> },
+    { name: 'Contact', link: 'contact', icon: <ContactIcon /> },
   ];
 
   return (
-    <AppBar position="sticky" style={{ backgroundColor: 'lightgrey', opacity: '98%' }}>
-      <Toolbar sx={{ minHeight: '50px' }}>
-        {isMobile ? (
-          <>
-            <IconButton edge="start" color="dark" aria-label="menu" onClick={handleMenuToggle}>
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
-              transformOrigin={{ vertical: 'top', horizontal: 'left' }}
-              anchorEl={null}
-              open={menuOpen}
-              onClose={closeMenu}
-              PaperProps={{
-                style: {
-                  backgroundColor: 'lightgrey',
-                },
-              }}
-            >
+    <>
+      {!isMobile && (
+        <AppBar position="sticky" style={{ backgroundColor: 'lightgrey', opacity: '98%' }}>
+          <Toolbar sx={{ minHeight: '50px' }}>
+            <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
               {navLinks.map((item, index) => (
-                <MenuItem key={index} onClick={closeMenu}>
-                  <Link to={item.link} smooth={true} offset={-70}>
-                    <ListItemText primary={item.name} />
-                  </Link>
-                </MenuItem>
+                <Link to={item.link} smooth={true} offset={-70} key={index}>
+                  <Button color="inherit" sx={{ mx: 5, color: 'black' }}>
+                    {item.name}
+                  </Button>
+                </Link>
               ))}
-            </Menu>
-          </>
-        ) : (
-          <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-            {navLinks.map((item, index) => (
-              <Link to={item.link} smooth={true} offset={-70} key={index}>
-                <Button color="inherit" sx={{ mx: 5, color: 'black' }}>
-                  {item.name}
-                </Button>
-              </Link>
-            ))}
-          </Box>
-        )}
-      </Toolbar>
-    </AppBar>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      )}
+{isMobile && (
+        <SpeedDial
+          ariaLabel="SpeedDial menu"
+          icon={speedDialOpen ? <MoreHorizIcon /> : <MenuIcon />} // Swap positions of MenuIcon and MoreVertIcon
+          onClose={handleSpeedDialClose}
+          onOpen={handleSpeedDialOpen}
+          open={speedDialOpen}
+          direction="down"
+          sx={{
+            position: 'fixed',
+            right: 0,
+            top: 0,
+            margin: '1rem',
+            transition: 'transform .5s',
+          }}
+          FabProps={{
+            style: { transform: speedDialOpen ? 'rotate(90deg)' : 'rotate(0deg)' },
+            sx: {
+              bgcolor: 'black',
+              opacity: '80%',
+              '&:hover': {
+                bgcolor: 'black',
+                opacity: '80%',
+              },
+            },
+          }}
+        >
+          {navLinks.map((action) => (
+            <SpeedDialAction
+              key={action.name}
+              onClick={handleSpeedDialClose}
+              icon={
+                <Link 
+                  to={action.link} 
+                  smooth={true} 
+                  offset={-70} 
+                  style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                >
+                  {action.icon}
+                </Link>
+              }
+            />
+          ))}
+        </SpeedDial>
+      )}
+    </>
   );
 };
 
